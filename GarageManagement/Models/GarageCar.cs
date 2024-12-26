a0bd98947f382289f6aab652da0145b5c88996cb
Niionik <patrycja.pl.opalacz@gmail.com>
2024-12-26 21:36:26 +0100

    GarageModel create

--- GarageManagement/Models/GarageCar.cs
+++ GarageManagement/Models/GarageCar.cs
@@ -1,11 +1,17 @@
-﻿namespace GarageManagement.Models
+﻿using GarageManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace GarageManagement.Models
 {
     public class GarageCar
     {
+        [Key]
         public int GarageId { get; set; }
-        public int CarId { get; set; }
 
+        [Key]
+        public int CarId { get; set; }
         public Garage Garage { get; set; }
         public Car Car { get; set; }
     }
 }
+
---
+++ GarageManagement/Models/GarageDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GarageManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace YourNamespace.Models
+{
+    public class GarageDbContext : DbContext
+    {
+        private const string V = "GarageDbContext";
+
+        public GarageDbContext() : base(V) { }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Maintenance> Maintenances { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Garage> Garages { get; set; }
+        public DbSet<GarageCar> GarageCars { get; set; }
+    }
+}
